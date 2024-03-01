cf76f7a46e39e95528c8c6147200283385b6ab65
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-01 16:22:44 -0500

    Movement updates

--- Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    public float horizontalInput;
+    public float forwardInput;
+    public float turnInput;
     public float speed = 0f;
     public float turnSpeed = 0f;
+    public float drift = 0.3f;
+    public float constantSpeed = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,13 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
+        forwardInput = Mathf.Clamp(Input.GetAxis("Vertical"),0,1);
+        turnInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * constantSpeed);
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * turnInput * Time.deltaTime * turnSpeed);
+
+        
     }
 }
