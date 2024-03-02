9d0f98442152865ea7a6e196dacd0d719ae9bdbf
Cooljerry96 <cooljerry96@yahoo.com>
2024-03-02 03:08:36 -0500

    Finished Background
    Finished Background

--- Alpha Project/Assets/Alex's ToolShed/Scripts/FollowUV.cs
+++ Alpha Project/Assets/Alex's ToolShed/Scripts/FollowUV.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowUV : MonoBehaviour
 {
-    float parrallax = 2f;
+    public float parrallax = 2f;
     
     // Start is called before the first frame update
     void Start()
