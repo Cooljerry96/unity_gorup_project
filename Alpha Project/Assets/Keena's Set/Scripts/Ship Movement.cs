693f4562846a1caf076435e8343f89dcd423457b
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-02 03:27:04 -0500

    movement script

--- Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
@@ -1,32 +1,58 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    public float forwardInput;
-    public float turnInput;
-    public float speed = 0f;
-    public float turnSpeed = 0f;
-    public float drift = 0.3f;
-    public float constantSpeed = 0.2f;
+    private bool forwardInput;
+    private bool isInputing => forwardInput;
+    private float turnInput;
+    public float speed = 1f;
+    public float turnSpeed = 2f;
+    public Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+
+    //  Awake is called when the script is first loaded, or when an object it is attached to is instantiated.
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
+
+
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        forwardInput = Mathf.Clamp(Input.GetAxis("Vertical"),0,1);
-        turnInput = Input.GetAxis("Horizontal");
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * constantSpeed);
-        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
-        transform.Rotate(Vector3.up * turnInput * Time.deltaTime * turnSpeed);
+        forwardInput = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            turnInput = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnInput = 1f;
+        }
+        else
+        {
+            turnInput = 0f;
+        }
+
+        if (forwardInput)
+        {
+            rb.AddForce(transform.forward * speed);
+        }
         
+
+        if (turnInput != 0f)
+        {
+            rb.AddTorque(transform.up * turnInput * turnSpeed);
+        }
+
+       
     }
+   
 }
+   
---
+++ Alpha Project/Assets/Keena's Set/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public float fireDelta = 0.1f;
+    public float animationDuration = 1f;
+
+    public float nextFire = 0.1f;
+    private GameObject newProjectile;
+    private float myTime = 0.0f;
+
+    void Update()
+    {
+        //Pew Pew
+        myTime = myTime + Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+            //Start Repeat
+            StartCoroutine(AnimateProjectile(newProjectile));
+
+            nextFire = nextFire - myTime;
+            myTime = 0.0f;
+        }
+    }
+
+    // Animation
+    IEnumerator AnimateProjectile(GameObject projectile)
+    {
+        float elapsedTime = 0;
+        Vector3 startPosition = projectile.transform.position;
+        Vector3 endPosition = startPosition + transform.forward * 3;
+
+        //Fire direction im looking
+
+        while (elapsedTime < animationDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            projectile.transform.position = Vector3.Lerp(startPosition, endPosition, (elapsedTime / animationDuration));
+            yield return null;
+        }
+        //Get that shit out my house # SUCK IT ALEX
+        Destroy(projectile);
+    }
+}
