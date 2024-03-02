4c3e6494d683180c6d38aa365a8b9ddb545072ed
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-02 14:41:02 -0500

    Teleport 2.0

--- Alpha Project/Assets/Keena's Set/Scripts/Out of Bound TP.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Out of Bound TP.cs
@@ -5,26 +5,32 @@ using UnityEngine.UIElements;
 
 public class OutofBoundTP : MonoBehaviour
 {
-    private float topBound = 3.9f;
-    private float lowerBound = -3.9f;
-    private float rightBound = 6.9f;
-    private float leftBound = -6.9f;
+    //Top and bottom Bounds
+    private float topBound = 100f;
+    private float lowerBound = -100f;
+
+    //Left Right Bounds
+    private float rightBound = 100f;
+    private float leftBound = -100f;
+    //RigidBody Variable
     public Rigidbody rb;
 
     // Start is called before the first frame update
     private void Awake()
     {
+        //makes the RigidBody Constatly run
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Position Variables
         float x = rb.position.x;
         float y = rb.position.y;
         float z = rb.position.z;
 
+        //Teleports
         if (transform.position.z > topBound)
         {
             rb.position = new Vector3(x, y, lowerBound);
--- Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Ship Movement.cs
@@ -14,7 +14,7 @@ public class ShipMovement : MonoBehaviour
     public float forwardDrift = 0.5f;
     public float turnDrift = 1f;
     public Rigidbody rb;
-
+    public float constantSpeed = .5f ;
 
     //  Awake is called when the script is first loaded, or when an object it is attached to is instantiated.
     private void Awake()
@@ -42,17 +42,20 @@ public class ShipMovement : MonoBehaviour
             turnInput = 0f;
         }
 
+        transform.Translate(Vector3.forward * constantSpeed);
+
         if (forwardInput)
         {
+            transform.Translate(Vector3.forward * constantSpeed);
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            rb.AddForce(transform.forward * forwardDrift);
+            //rb.AddForce(transform.forward * forwardDrift);
         }
         
 
         if (turnInput != 0f)
         {
             transform.Rotate(Vector3.up * turnInput * turnSpeed);
-            rb.AddTorque(transform.up * turnInput * turnDrift);
+           // rb.AddTorque(transform.up * turnInput * turnDrift);
         }
 
        
