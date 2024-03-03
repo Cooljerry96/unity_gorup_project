1829f701c0c39935a45fb7262fddf4dd4434543a
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-02 20:11:09 -0500

    k

--- Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour
+public class AsteroidSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Asteroid asteroidPrefab;
+    public float spawnDistance = 12f;
+    public float spawnRate = 1f;
+    public int amountPerSpawn = 1;
+    [Range(0f, 45f)]
+    public float trajectoryVariance = 15f;
+
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Spawn()
     {
-        
+        for (int i = 0; i < amountPerSpawn; i++)
+        {
+            // Choose a random direction from the center of the spawner and
+            // spawn the asteroid a distance away
+            Vector3 spawnDirection = Random.insideUnitCircle.normalized;
+            Vector3 spawnPoint = transform.position + (spawnDirection * spawnDistance);
+
+            // Calculate a random variance in the asteroid's rotation which will
+            // cause its trajectory to change
+            float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
+            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+
+            // Create the new asteroid by cloning the prefab and set a random
+            // size within the range
+            GameObject asteroid = Instantiate(asteroidPrefab, spawnPoint, rotation);
+            asteroid.size = Random.Range(asteroid.minSize, asteroid.maxSize);
+
+            // Set the trajectory to move in the direction of the spawner
+            Vector3 trajectory = rotation * -spawnDirection;
+            asteroid.SetTrajectory(trajectory);
+        }
     }
+
 }
