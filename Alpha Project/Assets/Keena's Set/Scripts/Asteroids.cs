0c16f7419970f50a58ec66255a3f921145974418
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-03 17:36:18 -0500

    spwn

--- Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
@@ -1,25 +1,25 @@
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
+
 [RequireComponent(typeof(Rigidbody))]
 public class Asteroid : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+   
     public Rigidbody rb;
 
     [SerializeField]
-    public GameObject[] sprites;
+    public GameObject[] asteroids;
 
     public float size = 1f;
     public float minSize = 0.35f;
     public float maxSize = 1.65f;
-    public float movementSpeed = 50f;
+    public float movementSpeed = .25f;
     public float maxLifetime = 30f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        
     }
 
     private void Start()
@@ -34,7 +34,10 @@ public class Asteroid : MonoBehaviour
         // Destroy the asteroid after it reaches its max lifetime
         Destroy(gameObject, maxLifetime);
     }
-
+    private void Update()
+    {
+        //transform.Translate(Vector3.forward * movementSpeed);
+    }
     public void SetTrajectory(Vector3 direction)
     {
         // The asteroid only needs a force to be added once since they have no
--- Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
@@ -1,38 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public Asteroid asteroidPrefab;
+    public Rigidbody rb;
+    public Asteroid[] asteroidPrefab;
     public float spawnDistance = 50f;
     public float spawnRate = 1f;
     public int amountPerSpawn = 1;
-    [Range(-45f, 45f)]
     public float trajectoryVariance = 15f;
+    private Vector3[] vectors = new Vector3[4];
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void Start()
     {
         InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
+    public void Update()
+    {
+        
+    }
 
     public void Spawn()
     {
+        float leftRange = rb.position.x - 50;
+        float rightRange = rb.position.x + 50;
+        float topRange = rb.position.z + 50;
+        float bottomRange = rb.position.y - 50;
+       
+
         for (int i = 0; i < amountPerSpawn; i++)
         {
+      
             // Choose a random direction from the center of the spawner and
             // spawn the asteroid a distance away
-            Vector3 spawnDirection = Random.insideUnitCircle.normalized;
-            Vector3 spawnPoint = new Vector3(Random.Range(-spawnDistance,spawnDistance), 0,Random.Range(-spawnDistance,spawnDistance));
-
+            Vector3 spawnDirection = Random.insideUnitSphere;
+            vectors[0] = new Vector3(leftRange, 0f , Random.Range(topRange,bottomRange));
+            vectors[1] = new Vector3(rightRange, 0f, Random.Range(topRange, bottomRange));
+            vectors[2] = new Vector3(Random.Range(leftRange, rightRange), 0f, topRange);
+            vectors[3] = new Vector3(Random.Range(leftRange, rightRange), 0f, bottomRange);
+            
             // Calculate a random variance in the asteroid's rotation which will
             // cause its trajectory to change
             float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
-            Quaternion rotation = Quaternion.AngleAxis(variance, spawnPoint);
+            Quaternion rotation = Quaternion.AngleAxis(variance, vectors[Random.Range(0,4)]);
 
             // Create the new asteroid by cloning the prefab and set a random
             // size within the range
-            Asteroid asteroid = Instantiate(asteroidPrefab, spawnPoint, rotation);
+            Asteroid asteroid = Instantiate(asteroidPrefab[Random.Range(0, asteroidPrefab.Length)], vectors[Random.Range(0,4)], rotation);
             asteroid.size = Random.Range(asteroid.minSize, asteroid.maxSize);
 
             
