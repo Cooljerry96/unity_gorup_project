ebae074944294c0fc9799639f6c69c56e2096b3e
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-02 22:40:13 -0500

    final today

--- Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroids : MonoBehaviour
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody))]
+public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer spriteRenderer;
+    public Rigidbody rb;
+
+    [SerializeField]
+    public GameObject[] sprites;
+
+    public float size = 1f;
+    public float minSize = 0.35f;
+    public float maxSize = 1.65f;
+    public float movementSpeed = 50f;
+    public float maxLifetime = 30f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
+        
+        // Set the scale and mass of the asteroid based on the assigned size so
+        // the physics is more realistic
+        transform.localScale = Vector3.one * size;
+        rb.mass = size;
+
+        // Destroy the asteroid after it reaches its max lifetime
+        Destroy(gameObject, maxLifetime);
     }
+
+    public void SetTrajectory(Vector3 direction)
+    {
+        // The asteroid only needs a force to be added once since they have no
+        // drag to make them stop moving
+        rb.AddForce( direction * movementSpeed);
+    }
+
 }
--- Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Spawn.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidSpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     public Asteroid asteroidPrefab;
-    public float spawnDistance = 12f;
+    public float spawnDistance = 50f;
     public float spawnRate = 1f;
     public int amountPerSpawn = 1;
-    [Range(0f, 45f)]
+    [Range(-45f, 45f)]
     public float trajectoryVariance = 15f;
 
     private void Start()
@@ -23,21 +23,19 @@ public class AsteroidSpawner : MonoBehaviour
             // Choose a random direction from the center of the spawner and
             // spawn the asteroid a distance away
             Vector3 spawnDirection = Random.insideUnitCircle.normalized;
-            Vector3 spawnPoint = transform.position + (spawnDirection * spawnDistance);
+            Vector3 spawnPoint = new Vector3(Random.Range(-spawnDistance,spawnDistance), 0,Random.Range(-spawnDistance,spawnDistance));
 
             // Calculate a random variance in the asteroid's rotation which will
             // cause its trajectory to change
             float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
-            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+            Quaternion rotation = Quaternion.AngleAxis(variance, spawnPoint);
 
             // Create the new asteroid by cloning the prefab and set a random
             // size within the range
-            GameObject asteroid = Instantiate(asteroidPrefab, spawnPoint, rotation);
+            Asteroid asteroid = Instantiate(asteroidPrefab, spawnPoint, rotation);
             asteroid.size = Random.Range(asteroid.minSize, asteroid.maxSize);
 
-            // Set the trajectory to move in the direction of the spawner
-            Vector3 trajectory = rotation * -spawnDirection;
-            asteroid.SetTrajectory(trajectory);
+            
         }
     }
 
