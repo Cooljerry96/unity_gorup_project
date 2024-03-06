f353ec2fdb6bc36c7fa3b23206510777e0cc4914
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-03-05 19:24:15 -0500

    hh

--- Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
+++ Alpha Project/Assets/Keena's Set/Scripts/Asteroids.cs
@@ -8,13 +8,14 @@ public class Asteroid : MonoBehaviour
     public Rigidbody rb;
 
     [SerializeField]
-    
 
+    public GameObject player;
     public float size = 1f;
     public float minSize = 0.35f;
     public float maxSize = 1.65f;
     public float movementSpeed = .25f;
     public float maxLifetime = 30f;
+   
 
     private void Awake()
     {
@@ -26,21 +27,40 @@ public class Asteroid : MonoBehaviour
     {
         
         
-        // Set the scale and mass of the asteroid based on the assigned size so
+        // Set the scale and mass of the GameObject based on the assigned size so
         // the physics is more realistic
         transform.localScale = Vector3.one * size;
         rb.mass = size;
 
-        // Destroy the asteroid after it reaches its max lifetime
-        Destroy(gameObject, maxLifetime);
+        
+       
     }
     private void Update()
     {
         transform.Translate(Vector3.forward * movementSpeed);
+
+        if (this.transform.position.x > player.transform.position.x + 110)
+        {
+            Destroy(gameObject);
+        }
+        if (this.transform.position.x < player.transform.position.x - 110)
+        {
+            Destroy(gameObject);
+        }
+        if (this.transform.position.z > player.transform.position.z + 110)
+        {
+            Destroy(gameObject);
+        }
+        if (this.transform.position.z < player.transform.position.z - 110)
+        {
+            Destroy(gameObject);
+        }
+        
+        
     }
     public void SetTrajectory(Vector3 direction)
     {
-        // The asteroid only needs a force to be added once since they have no
+        // The GameObject only needs a force to be added once since they have no
         // drag to make them stop moving
         rb.AddForce( direction * movementSpeed);
     }
